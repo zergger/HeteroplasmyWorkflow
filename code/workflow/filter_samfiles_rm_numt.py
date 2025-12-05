262bc7f1ff39ae1a19e69a3d67ab8bcdabdbea50
zergger <zergger@foxmail.com>
2025-12-05 15:46:27 +0100

    update

--- code/workflow/filter_samfiles_rm_numt.py
+++ code/workflow/filter_samfiles_rm_numt.py
@@ -2,64 +2,77 @@ import sys
 import os
 
 def filter_reads_for_cp_mt(filtered_sam, SAMdir, chloroplast, mitochondria):
-	genome_name = {}
-	if mitochondria != 'None':
-		genome_name['mitochondria'] = mitochondria.split(",")
-	if chloroplast != 'None':
-		genome_name['chloroplast'] = chloroplast.split(",")
+    """
+    Filters a SAM file for chloroplast or mitochondria reads, 
+    preserving the SAM header in the output files.
+    """
+    genome_name = {}
+    if mitochondria != 'None':
+        genome_name['mitochondria'] = mitochondria.split(",")
+    if chloroplast != 'None':
+        genome_name['chloroplast'] = chloroplast.split(",")
 
-	if len(genome_name) > 0:
-		outfilename = filtered_sam.split("/")[-1]
+    if len(genome_name) > 0:
+        outfilename = filtered_sam.split("/")[-1]
 
-		if chloroplast != 'None':
-			cp_out = os.path.join(SAMdir, 'chloroplast')
-			if not os.path.exists(cp_out):
-				os.makedirs(cp_out)
-			cp_outfile = os.path.join(cp_out, outfilename)
-			cpf = open(cp_outfile, 'w')
+        if chloroplast != 'None':
+            cp_out = os.path.join(SAMdir, 'chloroplast')
+            if not os.path.exists(cp_out):
+                os.makedirs(cp_out)
+            cp_outfile = os.path.join(cp_out, outfilename)
+            cpf = open(cp_outfile, 'w')
 
-		if mitochondria != 'None':
-			mt_out = os.path.join(SAMdir, 'mitochondria')
-			if not os.path.exists(mt_out):
-				os.makedirs(mt_out)
-			mt_outfile = os.path.join(mt_out, outfilename)
-			mtf =  open(mt_outfile, 'w')
+        if mitochondria != 'None':
+            mt_out = os.path.join(SAMdir, 'mitochondria')
+            if not os.path.exists(mt_out):
+                os.makedirs(mt_out)
+            mt_outfile = os.path.join(mt_out, outfilename)
+            mtf =  open(mt_outfile, 'w')
 
-		with open(filtered_sam, "r") as file:
-			for line in file:
-				if line[0] != '@':
-					items = line.split('\t')
-					ref_name = items[2]
-					sam_flag, ref_pos, cigar, read, phred = int(items[1]), int(items[3])-1, items[5], items[9], items[10]
+        with open(filtered_sam, "r") as file:
+            for line in file:
+                # [FIX] First, check if it's a header line
+                if line.startswith('@'):
+                    # Write the header line to all active output files
+                    if chloroplast != 'None':
+                        cpf.write(line)
+                    if mitochondria != 'None':
+                        mtf.write(line)
+                    # Continue to the next line
+                    continue
 
-					if 'S' not in cigar:
-						name_parts = ref_name.split("|")
-						for n in name_parts:
-							if chloroplast != 'None' and n in genome_name['chloroplast']:
-								# if 'XA:' not in line and f'XA:Z:{n}' not in line:
-								if 'XA:' not in line:
-									cpf.write(line)
-									break
+                items = line.split('\t')
+                ref_name = items[2]
+                sam_flag, ref_pos, cigar, read, phred = int(items[1]), int(items[3])-1, items[5], items[9], items[10]
 
-							if mitochondria != 'None' and n in genome_name['mitochondria']:
-								# if 'XA:' not in line and f'XA:Z:{n}' not in line:
-								if 'XA:' not in line:
-									mtf.write(line)
-									break
+                if 'S' not in cigar:
+                    name_parts = ref_name.split("|")
+                    for n in name_parts:
+                        if chloroplast != 'None' and n in genome_name['chloroplast']:
+                            # if 'XA:' not in line and f'XA:Z:{n}' not in line:
+                            if 'XA:' not in line:
+                                cpf.write(line)
+                                break
 
-		if chloroplast != 'None':
-			cpf.close()
-		if mitochondria != 'None':
-			mtf.close()
+                        if mitochondria != 'None' and n in genome_name['mitochondria']:
+                            # if 'XA:' not in line and f'XA:Z:{n}' not in line:
+                            if 'XA:' not in line:
+                                mtf.write(line)
+                                break
+
+        if chloroplast != 'None':
+            cpf.close()
+        if mitochondria != 'None':
+            mtf.close()
 
 if __name__ == '__main__':
-	if len(sys.argv) != 5:
-		print('Usage: python', sys.argv[0], 'filtered_sam_file','SAM_files_directory', 'chloroplast_ID', 'mitochondria_ID')
-		sys.exit(0)
+    if len(sys.argv) != 5:
+        print('Usage: python', sys.argv[0], 'filtered_sam_file','SAM_files_directory', 'chloroplast_ID', 'mitochondria_ID')
+        sys.exit(0)
 
-	filtered_sam = sys.argv[1] # filtered SAM file
-	SAMdir = sys.argv[2] # dir contains all sam files
-	chloroplast = sys.argv[3]
-	mitochondria = sys.argv[4]
+    filtered_sam = sys.argv[1] # filtered SAM file
+    SAMdir = sys.argv[2] # dir contains all sam files
+    chloroplast = sys.argv[3]
+    mitochondria = sys.argv[4]
 
-	filter_reads_for_cp_mt(filtered_sam, SAMdir, chloroplast, mitochondria)
+    filter_reads_for_cp_mt(filtered_sam, SAMdir, chloroplast, mitochondria)
